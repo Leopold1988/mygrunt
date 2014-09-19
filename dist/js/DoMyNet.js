4bee93605190c81f46c60b7699811b57e7786060
LiPeng <lipeng@ezlink.us>
2014-09-19 17:52:12 +0800

    grunt 测试版本。 "grunt": "~0.4.2",     "load-grunt-tasks": "~0.1.0",     "grunt-contrib-watch": "~0.5.3",     "grunt-contrib-copy": "~0.4.1",     "grunt-contrib-concat": "~0.3.0",     "grunt-contrib-clean": "~0.5.0",     "grunt-contrib-uglify": "~0.2.7",     "grunt-contrib-less": "~0.11.4",     "grunt-contrib-jade": "~0.12.0",     "grunt-contrib-jshint": "~0.10.0",     "grunt-contrib-connect": "~0.8.0",     "grunt-contrib-imagemin": "~0.8.1"

---
+++ dist/js/DoMyNet.js
@@ -0,0 +1,4 @@
+/*! DoMyNet 0.1.0 2014-09-19 */
+(function(){
+  var a = "abc";
+})();;var b = 2;;window.alert(11);;var a = 1;
---
+++ tasks/options/watch.js
@@ -0,0 +1,6 @@
+module.exports = exports = {
+  src: {
+    files: ['src/js/*.js', 'src/less/*.less', 'src/jade/**'],
+    tasks: ['jshint:with_overrides', 'clean:build', 'copy', 'concat', 'uglify:buildall', 'less:production', 'jade:release', 'imagemin:dynamic']
+  }
+};
